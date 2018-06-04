2e6452c6e94fe64bf3cb3c02a13e41b0f2c32001
jonathanport <jon@jportwebdesign.co.uk>
2018-06-04 21:54:02 +0100

    Fixed build for NPM

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'BetterEventListener.js',
     libraryExport: 'default',
-    libraryTarget: 'var',
+    libraryTarget: 'umd',
     library: 'BetterEventListener'
   },
   module: {
